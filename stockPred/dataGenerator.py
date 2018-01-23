43001f8e4491c9bfdda26a95ea45254961a952c1
wangzezhong <904192625@qq.com>
2018-01-23 04:28:13 -0500

    Create utility function which is widly used.

--- stockPred/dataGenerator.py
+++ stockPred/dataGenerator.py
@@ -1,10 +1,14 @@
+import os
+import pandas as pd
+from util import *
 
 class DataGenerator(object):
     # ze zhong wang
+    def __init__(self, file_path, code):
+        self.file_path = file_path
+        self.code = code
+        self.file_list = self.get_file_list(file_path)
+
     def run(self):
-
-
-
-
-
+        
         return train_feature, train_label, test_feature, test_label
